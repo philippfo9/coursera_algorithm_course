f5d0a473b3e751974b48ea544aa0dbafeedd17da
Philipp Fock <philipp.fock18@gmail.com>
2018-05-30 21:55:50 +0200

    quicksort-second

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -1,4 +1,6 @@
-
+Array.prototype.move = function (from, to) {
+    this.splice(to, 0, this.splice(from, 1)[0]);
+};
 
 function quickSort(arr) {
     if(arr.length <= 1) {
@@ -10,20 +12,30 @@ function quickSort(arr) {
 }
 
 function partitionAroundPivot(arr, pivot) {
-    let firstpart = [];
-    let secondpart = [];
-    arr.forEach(el => {
+    let secondPartBoundary = 0;
+    for(let m = 1; m < arr.length; m++) {
+        let el = arr[m];
         if(el < pivot) {
-            firstpart.push(el);
-        } else if(el > pivot) {
-            secondpart.push(el);
+            secondPartBoundary++;
+            arr.move(m, secondPartBoundary);
         }
-    });
-    firstpart = quickSort(firstpart);
-    secondpart = quickSort(secondpart);
-    let ret = firstpart;
-    ret.push(pivot);
-    return ret.concat(secondpart);
+    }
+    console.log("pivot", pivot);
+    console.log("arr to splice", arr);
+    console.log("boundary", secondPartBoundary);
+    let firstpart = arr.splice(1, secondPartBoundary);
+    console.log("firstpart", firstpart);
+    let secondpart = arr.splice(1);
+    console.log("secondpart", secondpart);
+    quickSort(firstpart);
+    quickSort(secondpart);
+    console.log("arr before", arr);
+    console.log("firstpart sorted", firstpart);
+    console.log("secondpart sorted", secondpart);
+    arr.unshift.apply(arr, firstpart);
+    arr.push.apply(arr, secondpart);
+    console.log("arr", arr);
+    return arr;
 }
 
 function choosePivot(arr) {
@@ -33,5 +45,5 @@ function choosePivot(arr) {
 }
 
 
-let b = [11, 3, 5, 1, 8, 10, 9, 7];
+let b = [14, 12, 11, 3, 5, 1, 13, 8, 10, 9, 7, 2, 6];
 console.log(quickSort(b));
